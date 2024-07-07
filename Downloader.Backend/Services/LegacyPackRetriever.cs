5aa4368d29426e5ff94d1216ddaa9d498673a650
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-07-07 23:46:09 +0200

    removed inconsistent services

--- Downloader.Backend/Services/LegacyPackRetriever.cs
+++
@@ -1,6 +0,0 @@
-namespace Downloader.Backend.Services;
-
-public class LegacyPackRetriever
-{
-    
-}
--- Downloader.Infrastructure/Services/ILegacyPackRetriever.cs
+++
@@ -1,6 +0,0 @@
-namespace Downloader.Infrastructure.Services;
-
-public interface ILegacyPackRetriever
-{
-    
-}
