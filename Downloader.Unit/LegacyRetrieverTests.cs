562790b36ed5821fa2d7a56ac783ca3a28f2a963
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-07-07 23:46:30 +0200

    rename

--- Downloader.Unit/LegacyRetrieverTests.cs
+++ Downloader.Unit/LegacyRetrieverTests.cs
@@ -7,7 +7,7 @@ public class LegacyRetrieverTests
     [Fact]
     public void LRT_0001_Given_CorrectUrl_When_GetModsCalled_Then_ReturnsMods()
     {
-        var legacyRetriever = new LegacyModRetriever();
+        var legacyRetriever = new LegacyService();
         var urls = Downloader.Infrastructure.Constants.GetLegacyModUrls();
         
         var mods = legacyRetriever.GetMods(urls);
