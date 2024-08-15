8be700469aa0e894fb26f3c72a30b70dadc58d78
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-08-15 14:27:06 +0200

    added working unit tests

--- Downloader.Unit/LegacyServiceTests.cs
+++ Downloader.Unit/LegacyServiceTests.cs
@@ -8,9 +8,22 @@ public class LegacyServiceTests
     public void LST_0001_Given_CorrectUrl_When_GetModsCalled_Then_ReturnsMods()
     {
         var legacyRetriever = new LegacyService();
-        var urls = Downloader.Infrastructure.Constants.GetLegacyModUrls();
+        var urls = Infrastructure.Constants.GetLegacyModUrls();
         
         var mods = legacyRetriever.GetMods(urls);
+        
+        Assert.NotNull(mods);
+        Assert.NotEmpty(mods);
+    }
+    
+    
+    [Fact]
+    public async void LST_0002_Given_CorrectUrl_When_GetModsAsyncCalled_Then_ReturnsMods()
+    {
+        var legacyRetriever = new LegacyService();
+        var urls = Infrastructure.Constants.GetLegacyModUrls();
+        
+        var mods = await legacyRetriever.GetModsAsync(urls);
         
         Assert.NotNull(mods);
         Assert.NotEmpty(mods);
@@ -20,7 +33,7 @@ public class LegacyServiceTests
     public void LST_0011_Given_CorrectUrl_When_GetModPacksCalled_Then_ReturnsModPacks()
     {
         var legacyRetriever = new LegacyService();
-        var url = Downloader.Infrastructure.Constants.GetLegacyModPackUrl();
+        var url = Infrastructure.Constants.GetLegacyModPackUrl();
         
         var modPacks = legacyRetriever.GetModPacks(url);
         
