e4ce9b7c3f860bf587fd080dff97a664f61628c2
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-08-15 10:51:49 +0200

    separated functions to async and non async and added summaries

--- Downloader.Infrastructure/Services/ILegacyService.cs
+++ Downloader.Infrastructure/Services/ILegacyService.cs
@@ -4,7 +4,31 @@ namespace Downloader.Infrastructure.Services;
 
 public interface ILegacyService
 {
+    /// <summary>
+    /// Retrieve a list of mods from the given urls using async.
+    /// </summary>
+    /// <param name="urls"></param>
+    /// <returns></returns>
+    Task<IEnumerable<IBaseMod>> GetModsAsync(IEnumerable<string> urls);
+    
+    /// <summary>
+    /// Retrieve a list of mods from the given urls.
+    /// </summary>
+    /// <param name="urls"></param>
+    /// <returns></returns>
     IEnumerable<IBaseMod> GetMods(IEnumerable<string> urls);
     
+    /// <summary>
+    /// Retrieve a list of mod packs from the given url using async.
+    /// </summary>
+    /// <param name="url"></param>
+    /// <returns></returns>
+    Task<IEnumerable<ILegacyModPack>> GetModPacksAsync(string url);
+    
+    /// <summary>
+    /// Retrieve a list of mod packs from the given url.
+    /// </summary>
+    /// <param name="url"></param>
+    /// <returns></returns>
     IEnumerable<ILegacyModPack> GetModPacks(string url);
 }
