69961352c8f5c82d6136a32a312bda5130c57549
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-08-22 23:35:03 +0200

    added summaries to IFile

--- Downloader.Infrastructure/Models/IFile.cs
+++ Downloader.Infrastructure/Models/IFile.cs
@@ -1,11 +1,37 @@
 namespace Downloader.Infrastructure.Models;
 
+/// <summary>
+/// IFile is managed by the IFileManager.
+/// </summary>
 public interface IFile
 {
+    /// <summary>
+    /// Source of the file.
+    /// </summary>
     string DownloadUrl { get; }
+    
+    /// <summary>
+    /// The actual file path on disk. (It can be either a temporary file or the final file path)
+    /// </summary>
     string FilePathOnDisk { get; }
+    
+    /// <summary>
+    /// If the file is downloaded. It can be used to prevent downloading the same file multiple times.
+    /// </summary>
     bool IsDownloaded { get; }
     
+    /// <summary>
+    /// Downloads the file.
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     Task DownloadAsync(CancellationToken cancellationToken);
+    
+    /// <summary>
+    /// Downloads the file using the provided HttpClient.
+    /// </summary>
+    /// <param name="client"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     Task DownloadAsync(HttpClient client, CancellationToken cancellationToken);
 }
